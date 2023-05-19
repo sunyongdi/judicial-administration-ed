d9cd67fe75ad35d1b499b2376aab694740ef5ad8
sunyongdi <914767648@qq.com>
2023-05-19 11:10:55 +0800

    Initial commit

---
+++ el_dataset.py
@@ -0,0 +1,65 @@
+import torch
+from torch.utils.data import Dataset, DataLoader, RandomSampler
+# 这里要显示的引入BertFeature，不然会报错
+from el_preprocess import BertFeature
+from el_preprocess import ELProcessor
+from transformers import BertTokenizer
+import el_config
+
+
+class ELDataset(Dataset):
+    def __init__(self, features):
+        self.nums = len(features)
+        self.features = features
+
+    def __len__(self):
+        return self.nums
+
+    def __getitem__(self, index):
+        example = self.features[index]
+        data = {
+            'token_ids': torch.tensor(example.token_ids).long(),
+            'attention_masks': torch.tensor(example.attention_masks).float(),
+            'token_type_ids': torch.tensor(example.token_type_ids).long(),
+        }
+
+        data['seq_labels'] = torch.tensor(example.seq_labels).long()
+        data['entity_labels'] = torch.tensor(example.entity_labels).long()
+
+        return data
+
+if __name__ == '__main__':
+    args = el_config.Args().get_parser()
+    args.log_dir = './logs/'
+    args.max_seq_len = 256
+    args.bert_dir = '../model_hub/chinese-bert-wwm-ext/'
+    tokenizer = BertTokenizer.from_pretrained(args.bert_dir + 'vocab.txt')
+
+
+    import pickle
+    test_out = pickle.load(open('./data/law_data/test.pkl','rb'))
+    test_features, test_callback_info = test_out
+    test_dataset = ELDataset(test_features)
+    for i,data in enumerate(test_dataset):
+        print(data['token_ids'])
+        print(tokenizer.convert_ids_to_tokens(data['token_ids']))
+        print(data['attention_masks'])
+        print(data['token_type_ids'])
+        print(data['seq_labels'])
+        print(data['entity_labels'])
+        if i == 2:
+            break
+
+    args.eval_batch_size = 2
+    test_sampler = RandomSampler(test_dataset)
+    test_loader = DataLoader(dataset=test_dataset,
+                              batch_size=args.eval_batch_size,
+                              sampler=test_sampler,
+                              num_workers=2)
+    for step, test_data in enumerate(test_loader):
+        print(test_data['token_ids'].shape)
+        print(test_data['attention_masks'].shape)
+        print(test_data['token_type_ids'].shape)
+        print(test_data['seq_labels'])
+        print(test_data['entity_labels'])
+        break
